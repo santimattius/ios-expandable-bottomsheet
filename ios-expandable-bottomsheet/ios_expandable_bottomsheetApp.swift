b864ad9b73d1d6959a4026067b420e63726de10f
Santiago Mattiauda <santimattius@gmail.com>
2021-12-03 19:16:45 -0300

    Initial Commit

---
+++ ios-expandable-bottomsheet/ios_expandable_bottomsheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_expandable_bottomsheetApp.swift
+//  ios-expandable-bottomsheet
+//
+//  Created by Santiago Mattiauda on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_expandable_bottomsheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
